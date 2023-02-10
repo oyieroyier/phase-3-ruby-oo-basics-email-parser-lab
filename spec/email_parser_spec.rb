1bf2500879c54cdaaef0f192a958591e77b0eb20
Bob Oyier <boboyier92@gmail.com>
2023-02-10 17:18:36 +0300

    Completed assignment

--- spec/email_parser_spec.rb
+++ spec/email_parser_spec.rb
@@ -1,26 +1,36 @@
 describe "EmailAddressParser" do
-  describe '.new' do
-    it 'has an initialize method that takes in a list of emails' do
-      expect { EmailAddressParser.new("avi@test.com, arel@test.com") }.not_to raise_error
+  describe ".new" do
+    it "has an initialize method that takes in a list of emails" do
+      expect {
+        EmailAddressParser.new("avi@test.com, arel@test.com")
+      }.not_to raise_error
     end
   end
 
-  describe '#parse' do
+  describe "#parse" do
     it "parses comma-separated value (CSV) emails" do
-      expect(EmailAddressParser.new("avi@test.com, arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
+      expect(EmailAddressParser.new("avi@test.com, arel@test.com").parse).to eq(
+        %w[avi@test.com arel@test.com]
+      )
     end
 
     it "parses space delimited emails" do
-      expect(EmailAddressParser.new("avi@test.com arel@test.com").parse).to eq(["avi@test.com", "arel@test.com"])
+      expect(EmailAddressParser.new("avi@test.com arel@test.com").parse).to eq(
+        %w[avi@test.com arel@test.com]
+      )
     end
 
     it "parses both CSV and space delimited emails" do
       emails = "avi@test.com, arel@test.com test@avi.com, test@arel.com"
-      expect(EmailAddressParser.new(emails).parse).to eq(["avi@test.com", "arel@test.com", "test@avi.com", "test@arel.com"]) 
+      expect(EmailAddressParser.new(emails).parse).to eq(
+        %w[avi@test.com arel@test.com test@avi.com test@arel.com]
+      )
     end
 
-    it 'parses and removes duplicate emails' do
-      expect(EmailAddressParser.new("avi@test.com, avi@test.com").parse).to eq(["avi@test.com"])
+    it "parses and removes duplicate emails" do
+      expect(EmailAddressParser.new("avi@test.com, avi@test.com").parse).to eq(
+        ["avi@test.com"]
+      )
     end
   end
 end
